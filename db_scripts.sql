09d878f4e0f3a665782d69ac726e6833db92b1fb
ROHINIPRIYA <111354616+Rohinipriya07@users.noreply.github.com>
2022-08-23 11:53:42 +0530

    Update db_scripts.sql

--- db_scripts.sql
+++ db_scripts.sql
@@ -1,6 +1,6 @@
 create schema studentmgnt;
 
-CREATE TABLE  studentmgnt.student_details  (
+CREATE TABLE  student_details  (
    student_id  int NOT NULL AUTO_INCREMENT,
    student_name  varchar(45) DEFAULT NULL,
    aadhar_card_number  varchar(45) DEFAULT NULL,
@@ -8,9 +8,9 @@ CREATE TABLE  studentmgnt.student_details  (
    ph_number  varchar(45) DEFAULT NULL,
    class_name  varchar(45) DEFAULT NULL,
    section  varchar(45) DEFAULT NULL,
+   fee_paid  varchar(45) DEFAULT 'not paid',
   PRIMARY KEY ( student_id )
 ) ;
-
 
 CREATE TABLE studentmgnt.student_marks  (
    id  int NOT NULL AUTO_INCREMENT,
@@ -22,4 +22,3 @@ CREATE TABLE studentmgnt.student_marks  (
    language  varchar(45) DEFAULT NULL,
   PRIMARY KEY ( id )
 ) ;
-
